3a3d3615bceaa9ce96e7bcf00b8952af294d7d8e
Paul <aelythe@gmail.com>
2018-07-18 18:27:22 +0300

    first commit

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import TeleportContent from './content';
-import TeleportPage from './page';
-import TeleportComponent from './component';
-import TeleportProject from './page';
-export default TeleportProject;
-export { TeleportComponent, TeleportContent, TeleportPage };
